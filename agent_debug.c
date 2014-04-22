3bcd25d6d7aacbbb791622c13d2fed7765599553
chixyang <chixyang@gmail.com>
2014-04-22 15:56:44 +0800

    201404221554201404221554201404221554201404221554201404221554201404221554201404221554201404221554201404221554201404221554201404221554201404221554201404221554201404221554201404221554201404221554201404221554201404221554201404221554201404221554201404221554201404221554

---
+++ agent_debug.c
@@ -0,0 +1,32 @@
+
+#include "agent_debug.h"
+
+pthread_mutex_t log_write_lock = PTHREAD_MUTEX_INITIALIZER;
+
+const char * log_file = "/var/log/oilagent.log";
+
+int log_fd = -1;
+
+char *log_buf = NULL;
+
+//初始化日志文件
+int initLog()
+{
+	//打开日志文件
+	log_fd = open(log_file,O_WRONLY | O_APPEND);
+	if(log_fd < 0)
+	{
+		perror("open log file error");
+		return -1;
+	}
+	//分配日志缓存
+	log_buf = (char *)malloc(log_buf_len * sizeof(char));
+	if(log_buf == NULL)
+	{
+		perror("malloc log buf error");
+		return -1;
+	}
+
+	return log_fd;
+}
+
